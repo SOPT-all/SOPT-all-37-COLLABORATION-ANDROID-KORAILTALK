43c760159e9cf737d520618d6ddbae2d84d04f86
Nahyun Kim <hedghog1004@naver.com>
2025-11-17 22:20:39 +0900

    feat/#14: BasicTopAppBar 컴포넌트 구현

---
+++ app/src/main/java/org/sopt/korailtalk/core/designsystem/component/topappbar/KorailTalkBasicTopAppBar.kt
@@ -0,0 +1,54 @@
+package org.sopt.korailtalk.core.designsystem.component.topappbar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.korailtalk.core.designsystem.theme.KORAILTALKTheme
+import org.sopt.korailtalk.core.designsystem.theme.KorailTalkTheme
+
+@Composable
+fun KorailTalkBasicTopAppBar(
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable () -> Unit = {},
+    backgroundColor: Color = KorailTalkTheme.colors.primary700
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(backgroundColor)
+            .padding(horizontal = 4.dp)
+            .height(56.dp)
+    ) {
+        Row( // left content
+            modifier = Modifier.align(Alignment.CenterStart),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            navigationIcon()
+        }
+
+        Row( // right icons
+            modifier = Modifier.align(Alignment.CenterEnd),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            actions()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun KorailTalkBasicTopAppBarPreview() {
+    KORAILTALKTheme {
+        KorailTalkBasicTopAppBar()
+    }
+}
