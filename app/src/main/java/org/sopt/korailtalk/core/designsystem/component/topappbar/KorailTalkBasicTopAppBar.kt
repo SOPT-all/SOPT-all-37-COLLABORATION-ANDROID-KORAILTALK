c6b0ed7e85e26e6c3d58d979a270a460c64c4cb8
Nahyun Kim <hedghog1004@naver.com>
2025-11-17 22:23:22 +0900

    style/#14: BasicTopAppBar 프리뷰에 홈 TopAppBar 예시 추가

--- app/src/main/java/org/sopt/korailtalk/core/designsystem/component/topappbar/KorailTalkBasicTopAppBar.kt
+++ app/src/main/java/org/sopt/korailtalk/core/designsystem/component/topappbar/KorailTalkBasicTopAppBar.kt
@@ -1,17 +1,22 @@
 package org.sopt.korailtalk.core.designsystem.component.topappbar
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.sopt.korailtalk.R
 import org.sopt.korailtalk.core.designsystem.theme.KORAILTALKTheme
 import org.sopt.korailtalk.core.designsystem.theme.KorailTalkTheme
 
@@ -49,6 +54,25 @@ fun KorailTalkBasicTopAppBar(
 @Composable
 private fun KorailTalkBasicTopAppBarPreview() {
     KORAILTALKTheme {
-        KorailTalkBasicTopAppBar()
+        KorailTalkBasicTopAppBar(
+            navigationIcon = {
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.img_korail_logo),
+                    contentDescription = null,
+                )
+            },
+            actions = {
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_translate),
+                    contentDescription = null,
+                    modifier = Modifier.size(44.dp)
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_hamburger),
+                    contentDescription = null,
+                    modifier = Modifier.size(44.dp)
+                )
+            }
+        )
     }
 }
