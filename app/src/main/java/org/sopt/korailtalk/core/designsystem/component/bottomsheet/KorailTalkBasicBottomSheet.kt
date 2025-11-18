788c329a11f0053bb111ca8438fc095c03cde334
Kyu hyunSung <dmp100@naver.com>
2025-11-18 16:40:31 +0900

    feat/#6: 코드리뷰 반영

--- app/src/main/java/org/sopt/korailtalk/core/designsystem/component/bottomsheet/KorailTalkBasicBottomSheet.kt
+++ app/src/main/java/org/sopt/korailtalk/core/designsystem/component/bottomsheet/KorailTalkBasicBottomSheet.kt
@@ -1,5 +1,7 @@
 package org.sopt.korailtalk.core.designsystem.component.bottomsheet
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -29,16 +33,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
+import org.sopt.korailtalk.core.common.util.extension.noRippleClickable
+import org.sopt.korailtalk.core.designsystem.theme.KorailTalkTheme
 
 /**
  * 공통 Modal BottomSheet 컴포넌트
@@ -65,13 +67,47 @@ import kotlinx.coroutines.launch
  * @param content BottomSheet 내부 컨텐츠
  */
 
+
+
+@Preview
+@Composable
+private fun KorailTalkBasicBottomSheetPreview() {
+    var showSheet by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "BottomSheet 열기",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .noRippleClickable {
+                    showSheet = true
+                }
+        )
+
+        if (showSheet) {
+            KorailTalkBasicBottomSheet(
+                isVisible = showSheet,
+                onDismiss = { showSheet = false },
+            ) {
+                Text("내용")
+                Button(onClick = { showSheet = false }) {
+                    Text("닫기")
+                }
+            }
+        }
+    }
+}
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun KorailTalkBasicBottomSheet(
+    modifier: Modifier = Modifier,
     isVisible: Boolean,
     onDismiss: () -> Unit,
-    title: String? = null,
-    content: @Composable ColumnScope.() -> Unit
+    content: @Composable () -> Unit
 ) {
     val sheetState = rememberModalBottomSheetState()
 
@@ -79,27 +115,32 @@ fun KorailTalkBasicBottomSheet(
         ModalBottomSheet(
             onDismissRequest = onDismiss,
             sheetState = sheetState,
-            containerColor = Color.White,
+            dragHandle = {
+                // 커스텀 드래그 핸들러
+                Box(
+                    Modifier
+                        .padding(12.dp)
+                        .width(55.dp)
+                        .height(4.dp)
+                        .background(
+                            color = KorailTalkTheme.colors.gray300,
+                            shape = RoundedCornerShape(4.dp)
+                        )
+                )
+            } ,
+            containerColor = KorailTalkTheme.colors.white,
+            modifier = modifier,
         ) {
             Column(
-                modifier = Modifier
+                Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-                // 제목이 있으면 표시
-                title?.let {
-                    Text(
-                        text = it,
-                        style = MaterialTheme.typography.titleMedium,
-                        modifier = Modifier.padding(bottom = 16.dp)
-                    )
-                }
-
                 // 외부에서 주입받은 컨텐츠
                 content()
 
                 // 하단 여백 (제스처 영역 확보)
-                Spacer(modifier = Modifier.height(16.dp))
+                Spacer(Modifier.height(16.dp))
             }
         }
     }
