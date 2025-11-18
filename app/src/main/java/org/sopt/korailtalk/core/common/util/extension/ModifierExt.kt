d09a9a24de8a04e6840d212f9d826885b34c79ab
김종우 <kimjw111003@gmail.com>
2025-11-19 00:33:21 +0900

    feat/#24: 메뉴 바텀시트 완성

--- app/src/main/java/org/sopt/korailtalk/core/common/util/extension/ModifierExt.kt
+++ app/src/main/java/org/sopt/korailtalk/core/common/util/extension/ModifierExt.kt
@@ -10,7 +10,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 
 @Composable
 inline fun Modifier.noRippleClickable(
@@ -52,3 +59,146 @@ inline fun Modifier.pressedClickable(
         )
     }
 }
+
+/**
+ * topBorder = Border 사용처 중에 top만 필요한 경우 사용
+ * bottomBorder = Border 사용처 중에 bottom만 필요한 경우 사용
+ * sideBorder = Border 사용처 중에 side만 필요한 경우 사용
+ *
+ * 각 border는 실제 수치의 2배를 넣어줘야 함
+ * 이유) strokeWidth가 drawLine 및 drawArc에서 중앙을 기준으로 양쪽으로 그려지기 때문
+ * */
+
+fun Modifier.topBorder(strokeWidth: Dp, color: Color, cornerRadiusDp: Dp) = composed(
+    factory = {
+        val density = LocalDensity.current
+        val strokeWidthPx = density.run { strokeWidth.toPx() }
+        val cornerRadiusPx = density.run { cornerRadiusDp.toPx() }
+
+        Modifier.drawBehind {
+            val width = size.width
+            val height = size.height
+
+            drawLine(
+                color = color,
+                start = Offset(x = 0f, y = height+2),
+                end = Offset(x = 0f, y = cornerRadiusPx),
+                strokeWidth = strokeWidthPx
+            )
+
+            drawArc(
+                color = color,
+                startAngle = 180f,
+                sweepAngle = 90f,
+                useCenter = false,
+                topLeft = Offset.Zero,
+                size = Size(cornerRadiusPx * 2, cornerRadiusPx * 2),
+                style = Stroke(width = strokeWidthPx)
+            )
+
+            drawLine(
+                color = color,
+                start = Offset(x = cornerRadiusPx, y = 0f),
+                end = Offset(x = width - cornerRadiusPx, y = 0f),
+                strokeWidth = strokeWidthPx
+            )
+
+            drawArc(
+                color = color,
+                startAngle = 270f,
+                sweepAngle = 90f,
+                useCenter = false,
+                topLeft = Offset(x = width - cornerRadiusPx * 2, y = 0f),
+                size = Size(cornerRadiusPx * 2, cornerRadiusPx * 2),
+                style = Stroke(width = strokeWidthPx)
+            )
+
+            drawLine(
+                color = color,
+                start = Offset(x = width, y = height+2),
+                end = Offset(x = width, y = cornerRadiusPx),
+                strokeWidth = strokeWidthPx
+            )
+        }
+    }
+)
+
+fun Modifier.bottomBorder(strokeWidth: Dp, color: Color, cornerRadiusDp: Dp) = composed(
+    factory = {
+        val density = LocalDensity.current
+        val strokeWidthPx = density.run { strokeWidth.toPx() }
+        val cornerRadiusPx = density.run { cornerRadiusDp.toPx() }
+
+        Modifier.drawBehind {
+            val width = size.width
+            val height = size.height
+
+            drawLine(
+                color = color,
+                start = Offset(x = 0f, y = -2f),
+                end = Offset(x = 0f, y = height-cornerRadiusPx),
+                strokeWidth = strokeWidthPx
+            )
+
+            drawArc(
+                color = color,
+                startAngle = 90f,
+                sweepAngle = 90f,
+                useCenter = false,
+                topLeft = Offset(x = 0f, y = height - cornerRadiusPx * 2),
+                size = Size(cornerRadiusPx * 2, cornerRadiusPx * 2),
+                style = Stroke(width = strokeWidthPx)
+            )
+
+            drawLine(
+                color = color,
+                start = Offset(x = cornerRadiusPx, y = height),
+                end = Offset(x = width - cornerRadiusPx, y = height),
+                strokeWidth = strokeWidthPx
+            )
+
+            drawArc(
+                color = color,
+                startAngle = 0f,
+                sweepAngle = 90f,
+                useCenter = false,
+                topLeft = Offset(x = width - cornerRadiusPx * 2, y = height - cornerRadiusPx * 2),
+                size = Size(cornerRadiusPx * 2, cornerRadiusPx * 2),
+                style = Stroke(width = strokeWidthPx)
+            )
+
+            drawLine(
+                color = color,
+                start = Offset(x = width, y = -2f),
+                end = Offset(x = width, y = height - cornerRadiusPx),
+                strokeWidth = strokeWidthPx
+            )
+        }
+    }
+)
+
+fun Modifier.sideBorder(strokeWidth: Dp, color: Color) = composed(
+    factory = {
+        val density = LocalDensity.current
+        val strokeWidthPx = density.run { strokeWidth.toPx() }
+
+        Modifier.drawBehind {
+            val width = size.width
+            val height = size.height
+
+            drawLine(
+                color = color,
+                start = Offset(x = 0f, y = -2f),
+                end = Offset(x = 0f, y = height+2),
+                strokeWidth = strokeWidthPx
+            )
+
+            drawLine(
+                color = color,
+                start = Offset(x = width, y = -2f),
+                end = Offset(x = width, y = height+2),
+                strokeWidth = strokeWidthPx
+            )
+        }
+    }
+)
