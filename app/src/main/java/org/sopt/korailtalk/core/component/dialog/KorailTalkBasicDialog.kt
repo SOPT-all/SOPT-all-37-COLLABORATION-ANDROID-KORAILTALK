22a17634fe4c4aa0a7c6e6ce8c64117ce0e7c11c
oilbeaneda <20221444@sungshin.ac.kr>
2025-11-18 16:11:39 +0900

    reafactpr/#13-코리반영(dialog)

--- app/src/main/java/org/sopt/korailtalk/core/component/dialog/KorailTalkBasicDialog.kt
+++ app/src/main/java/org/sopt/korailtalk/core/component/dialog/KorailTalkBasicDialog.kt
@@ -1,4 +1,3 @@
-// 파일: org/sopt/korailtalk/core/component/dialog/KorailTalkBasicDialog.kt
 package org.sopt.korailtalk.core.component.dialog
 
 import androidx.compose.foundation.background
@@ -7,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,18 +15,24 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import org.sopt.korailtalk.core.common.util.extension.noRippleClickable
+import org.sopt.korailtalk.core.designsystem.theme.KORAILTALKTheme
 import org.sopt.korailtalk.core.designsystem.theme.KorailTalkTheme
 
 @Composable
 fun KorailTalkBasicDialog(
     onDismissRequest: () -> Unit,
     message: String,
-    buttons: @Composable BoxScope.() -> Unit
+    buttons: @Composable BoxScope.() -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Dialog(onDismissRequest = onDismissRequest) {
         KorailTalkBasicDialogContent(
@@ -39,7 +45,7 @@ fun KorailTalkBasicDialog(
 
 
 @Composable
-fun KorailTalkBasicDialogContent(
+private fun KorailTalkBasicDialogContent(
     message: String,
     buttons: @Composable BoxScope.() -> Unit,
     modifier: Modifier = Modifier
@@ -60,7 +66,7 @@ fun KorailTalkBasicDialogContent(
                     color = KorailTalkTheme.colors.white,
                     shape = RoundedCornerShape(size = 12.dp)
                 )
-                .padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
+                .padding(horizontal = 16.dp, vertical = 8.dp)
         ) {
             Text(
                 text = message,
@@ -76,6 +82,37 @@ fun KorailTalkBasicDialogContent(
             Box(
                 modifier = Modifier.fillMaxWidth(),
                 content = buttons
+            )
+        }
+    }
+}
+
+
+
+@Preview
+@Composable
+private fun Preview() {
+    var isBottomSheetVisible by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "다이얼로그 띄우기",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .noRippleClickable {
+                    isBottomSheetVisible = true
+                }
+        )
+
+        if (isBottomSheetVisible) {
+            KorailTalkBasicDialog(
+                onDismissRequest = { isBottomSheetVisible = false },
+                message = "메시지",
+                buttons = {
+                    // 버튼 추가
+                }
             )
         }
     }
