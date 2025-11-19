80dea5429b412186d40b69cce266c427d60a9002
Kyu hyunSung <dmp100@naver.com>
2025-11-19 19:17:59 +0900

    feat/#25-naming-component

---
+++ app/src/main/java/org/sopt/korailtalk/presentation/reservation/component/SeatTypeItem.kt
@@ -0,0 +1,19 @@
+package org.sopt.korailtalk.presentation.reservation.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun SeatTypeItem()
+{
+
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun SeatTypeItemPreview()
+{
+    SeatTypeItem()
+}
---
+++ app/src/main/java/org/sopt/korailtalk/presentation/reservation/component/TrainTypeNameChip.kt
@@ -0,0 +1,18 @@
+package org.sopt.korailtalk.presentation.reservation.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TrainTypeNameChip()
+{
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TrainTypeNameChipPreview()
+{
+    TrainTypeNameChip()
+}
+
