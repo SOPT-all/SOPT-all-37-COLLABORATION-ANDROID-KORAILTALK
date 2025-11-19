ff3a73df1da617e433fd74428e1be2f0a1eb144e
Nahyun Kim <hedghog1004@naver.com>
2025-11-19 23:46:25 +0900

    style/#22: CheckoutRow 프리뷰 테스트 코드 추가

--- app/src/main/java/org/sopt/korailtalk/presentation/checkout/component/row/CheckouBasicRow.kt
+++ app/src/main/java/org/sopt/korailtalk/presentation/checkout/component/row/CheckouBasicRow.kt
@@ -1,5 +1,6 @@
 package org.sopt.korailtalk.presentation.checkout.component.row
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +10,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,6 +46,9 @@ fun CheckboxBasicRow(
 @Composable
 private fun CheckboxRowPreview() {
     KORAILTALKTheme {
+        var couponText by remember { mutableStateOf("") }
+        var numberText by remember { mutableStateOf("") }
+
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier
@@ -49,16 +57,34 @@ private fun CheckboxRowPreview() {
         ) {
             CheckboxDropDownRow(
                 title = "할인 쿠폰",
-                onClick = {},
+                onClick = {}, // 바텀시트 노출 등 작업
                 placeholder = "적용할 쿠폰 선택",
+                selected = couponText,
                 modifier = Modifier.fillMaxWidth()
             )
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "할인쿠폰 1",
+                    modifier = Modifier
+                        .clickable { couponText = "할인쿠폰 1" }
+                )
+
+                Text(
+                    text = "팔인쿠폰 2",
+                    modifier = Modifier
+                        .clickable { couponText = "할인쿠폰 2" }
+                )
+            }
 
             CheckboxTextFieldRow(
                 title = "보훈 번호",
                 placeholder = "보훈 번호 9자리",
-                value = "",
-                onValueChange = {},
+                value = numberText,
+                onValueChange = { numberText = it },
                 modifier = Modifier.fillMaxWidth()
             )
         }
