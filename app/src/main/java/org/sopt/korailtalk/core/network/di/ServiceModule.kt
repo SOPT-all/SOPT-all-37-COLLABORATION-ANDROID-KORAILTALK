63b6d3d77cf70bc5a196ed5e0789266644f01286
Nahyun Kim <hedghog1004@naver.com>
2025-11-22 13:08:02 +0900

    delete: 더미 삭제

--- app/src/main/java/org/sopt/korailtalk/core/network/di/ServiceModule.kt
+++ app/src/main/java/org/sopt/korailtalk/core/network/di/ServiceModule.kt
@@ -4,10 +4,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import org.sopt.korailtalk.data.service.DummyService
 import org.sopt.korailtalk.data.service.KorailTalkApiService
 import retrofit2.Retrofit
-import javax.inject.Named
 import javax.inject.Singleton
 
 /**
@@ -16,11 +14,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
-
-    @Provides
-    @Singleton
-    fun provideDummyApi(retrofit: Retrofit): DummyService = retrofit.create(DummyService::class.java)
-
     @Provides
     @Singleton
     fun provideKorailTalkApi(retrofit: Retrofit): KorailTalkApiService = retrofit.create(KorailTalkApiService::class.java)
--- app/src/main/java/org/sopt/korailtalk/domain/repository/DummyRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.korailtalk.domain.repository
-
-import org.sopt.korailtalk.data.dto.request.DummyRequestDto
-import org.sopt.korailtalk.domain.model.DummyModel
-
-interface DummyRepository {
-    suspend fun dummy(requestDto: DummyRequestDto) : Result<DummyModel>
-}
