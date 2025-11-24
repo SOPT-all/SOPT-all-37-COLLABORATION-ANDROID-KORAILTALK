2c0bc8d4145739e1c789177c97862ad23ea0ff63
oilbeaneda <20221444@sungshin.ac.kr>
2025-11-24 18:50:34 +0900

    feat/#29-홈 스크린

--- app/src/main/java/org/sopt/korailtalk/presentation/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/korailtalk/presentation/home/HomeScreen.kt
@@ -1,121 +1,129 @@
 package org.sopt.korailtalk.presentation.home
 
-import android.R
+import org.sopt.korailtalk.R
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
-import org.sopt.korailtalk.core.common.util.extension.noRippleClickable
+import androidx.hilt.navigation.compose.hiltViewModel
 import org.sopt.korailtalk.core.common.util.preview.DefaultPreview
-import org.sopt.korailtalk.core.designsystem.component.button.KorailTalkBasicButton
 import org.sopt.korailtalk.core.designsystem.component.topappbar.KorailTalkBasicTopAppBar
 import org.sopt.korailtalk.core.designsystem.theme.KorailTalkTheme
+import org.sopt.korailtalk.presentation.home.component.CheckTrainCard
+import org.sopt.korailtalk.presentation.home.component.GridItemData
+import org.sopt.korailtalk.presentation.home.component.IconGridScreen
 
 @Composable
 fun HomeRoute(
     paddingValues: PaddingValues,
-    navigateToReservation: () -> Unit
+    navigateToReservation: (String,String) -> Unit
 ) {
-//    HomeScreenContent(
-//        modifier = Modifier.padding(paddingValues),
-//        //데이터 전달
-//        startStation = startStation,
-//        endStation = endStation,
-//        dateText = dateText,
-//        //이벤트 콜백 전달
-//        onReservationClick = navigateToReservation
-//
-//    )
+    HomeScreenContent(
+        modifier = Modifier.padding(paddingValues),
+        navigateToReservation = navigateToReservation
+
+    )
 }
 
-//UI 분리
+val items = listOf(
+    GridItemData(R.drawable.ic_navigation, "길안내"),
+    GridItemData(R.drawable.ic_location, "열차위치"),
+    GridItemData(R.drawable.ic_parking, "주차"),
+    GridItemData(R.drawable.ic_bus, "공항버스"),
+    GridItemData(R.drawable.ic_car, "렌터카"),
+    GridItemData(R.drawable.ic_car_sharing, "카셰어링"),
+    GridItemData(R.drawable.ic_carrier, "짐배송"),
+    GridItemData(R.drawable.ic_bakery, "카페&빵"),
+    GridItemData(R.drawable.ic_ticket, "레저이용권"),
+    GridItemData(R.drawable.ic_taxi, "관광택시")
+)
 @Composable
 private fun HomeScreenContent(
-    startStation: String,
-    endStation: String,
-    dateText: String,
-    onReservationClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel(),
+    navigateToReservation: (String,String) -> Unit
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+
+
     Column(
         modifier
-            .background(KorailTalkTheme.colors.white)
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(horizontal=20.dp,vertical=16.dp),
+            .background(KorailTalkTheme.colors.gray50)
+            .fillMaxSize(),
         horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
     ) {
-        KorailTalkBasicTopAppBar()
-        Text(
-            "어디로 갈까요, 민주 님?",
-            style = KorailTalkTheme.typography.headline.head4M18,
-            color = KorailTalkTheme.colors.primary400
-        )
-        Spacer(Modifier.height(24.dp))
-        //출발도착날짜인원조회 박스
-        Column(
-            verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.Top),
-            horizontalAlignment = Alignment.Start,
+        KorailTalkBasicTopAppBar(
+            navigationIcon = {
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.img_korail_logo),
+                    contentDescription = null,
+                )
+            },
+            actions = {
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_translate),
+                    contentDescription = null,
+                    modifier = Modifier.size(44.dp)
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_hamburger),
+                    contentDescription = null,
+                    modifier = Modifier.size(44.dp)
+                )
+            })
+        Row(
+            Modifier.padding(start = 24.dp, top = 24.dp, end = 24.dp, bottom = 16.dp)
         ) {
-            Modifier
-                .shadow(
-                    elevation = 12.dp,
-                    spotColor = KorailTalkTheme.colors.black,
-                    ambientColor = KorailTalkTheme.colors.black
-                )
-                .width(328.dp)
-                .height(324.dp)
-                .background(
-                    color = KorailTalkTheme.colors.white,
-                    shape = RoundedCornerShape(size = 10.dp)
-                )
-                .padding(start = 20.dp, top = 24.dp, end = 20.dp, bottom = 24.dp)
-            //출발
-            Row {
-                Text(
-                    text = "출발",
-                    style = KorailTalkTheme.typography.body.body2M15,
-                    color = KorailTalkTheme.colors.gray400
-                )
-
-            }
-            //도착
-            Row {
-
-            }
-            //날짜
-            Row {
-
-            }
-            //인원
-            Row {
-
-            }
-            //버튼
-            KorailTalkBasicButton(
-                buttonText = "열차조회",
-                onClick = onReservationClick,
-                Modifier.noRippleClickable(onClick = onReservationClick)
+            Text(
+                "어디로 갈까요,민주 님?",
+                style = KorailTalkTheme.typography.headline.head4M18,
+                color = KorailTalkTheme.colors.primary700,
             )
         }
+        Box(
+             Modifier.fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 20.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            CheckTrainCard(
+                startStation = uiState.startStation,
+                endStation = uiState.endStation,
+                onSwapClick = { viewModel.swapStations() }, // 뷰모델의 함수 호출
+                onReservationClick = { navigateToReservation(uiState.startStation, uiState.endStation) }
+            )
+        }
+        Spacer(Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            IconGridScreen(items = items)
+        }
+
     }
+
+
 }
 
 
@@ -123,9 +131,6 @@ private fun HomeScreenContent(
 @Composable
 private fun HomeScreenPreview() {
     HomeScreenContent(
-        startStation = "서울",
-        endStation = "부산",
-        dateText = "11.10 (월) · 14시 이후",
-        onReservationClick = {}
+        navigateToReservation = {_,_->}
     )
 }
