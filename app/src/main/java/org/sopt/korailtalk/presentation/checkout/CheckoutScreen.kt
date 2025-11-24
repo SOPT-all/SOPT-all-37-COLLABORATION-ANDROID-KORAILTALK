d67b4f395aac23291bc26799e5b3701083417754
Nahyun Kim <hedghog1004@naver.com>
2025-11-24 22:56:46 +0900

    feat/#39: Checkout 하단 fixed 영역 작업

--- app/src/main/java/org/sopt/korailtalk/presentation/checkout/CheckoutScreen.kt
+++ app/src/main/java/org/sopt/korailtalk/presentation/checkout/CheckoutScreen.kt
@@ -1,20 +1,30 @@
 package org.sopt.korailtalk.presentation.checkout
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import org.sopt.korailtalk.core.common.util.extension.noRippleClickable
 import org.sopt.korailtalk.core.common.util.preview.DefaultPreview
+import org.sopt.korailtalk.core.designsystem.theme.KorailTalkTheme
 import org.sopt.korailtalk.presentation.checkout.view.CheckoutBottomView
 import org.sopt.korailtalk.presentation.checkout.view.CheckoutTopView
+import java.text.NumberFormat
+import java.util.Locale
 
 @Composable
 fun CheckoutRoute(
@@ -36,7 +46,9 @@ private fun CheckoutScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .background(KorailTalkTheme.colors.white)
+            .fillMaxSize()
     ) {
         Row( //TODO: TopAppBar로 수정 (@kimjw2003)
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -67,7 +79,79 @@ private fun CheckoutScreen(
             }
         }
 
-        // TODO: 하단 Fixed 영역 작업 (@nahy-512)
+        FinalPriceInfo( // 총 결제 금액
+            price = 48800
+        )
+
+        BottomFixedButtons( // 예약취소 및 다음 버튼
+            onCancelClick = {},
+            onNextClick = {}
+        )
+    }
+}
+
+@Composable
+private fun FinalPriceInfo(
+    price: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .background(KorailTalkTheme.colors.primary700)
+            .fillMaxWidth()
+            .height(84.dp)
+            .padding(horizontal = 16.dp)
+    ) {
+        Text(
+            text = "총 결제 금액",
+            style = KorailTalkTheme.typography.body.body2M15,
+            color = KorailTalkTheme.colors.white
+        )
+        Text(
+            text = "${NumberFormat.getInstance(Locale.KOREA).format(price)}원",
+            style = KorailTalkTheme.typography.headline.head2M20,
+            color = KorailTalkTheme.colors.white
+        )
+    }
+}
+
+@Composable
+private fun BottomFixedButtons(
+    onCancelClick: () -> Unit,
+    onNextClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .background(KorailTalkTheme.colors.primary200)
+            .fillMaxWidth()
+            .height(56.dp)
+    ) {
+        Text(
+            text = "예약취소",
+            style = KorailTalkTheme.typography.body.body1R16,
+            color = KorailTalkTheme.colors.primary700,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .noRippleClickable(onClick = onCancelClick)
+                .weight(1f)
+                .fillMaxHeight()
+                .wrapContentHeight(Alignment.CenterVertically)
+        )
+        Text(
+            text = "다음",
+            style = KorailTalkTheme.typography.body.body1R16,
+            color = KorailTalkTheme.colors.primary700,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .noRippleClickable(onClick = onNextClick)
+                .weight(1f)
+                .fillMaxHeight()
+                .wrapContentHeight(Alignment.CenterVertically)
+        )
     }
 }
 
