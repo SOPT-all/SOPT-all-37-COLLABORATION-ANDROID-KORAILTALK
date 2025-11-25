cd6f7d9e16f42d27873d5e1cf0528fab7ca4ab2f
oilbeaneda <20221444@sungshin.ac.kr>
2025-11-25 19:33:34 +0900

    feat/#47-Repository 구현

--- app/src/main/java/org/sopt/korailtalk/data/repositoryImpl/KorailTalkRepositoryImpl.kt
+++ app/src/main/java/org/sopt/korailtalk/data/repositoryImpl/KorailTalkRepositoryImpl.kt
@@ -4,6 +4,7 @@ import org.sopt.korailtalk.data.mapper.toDto
 import org.sopt.korailtalk.data.mapper.toModel
 import org.sopt.korailtalk.data.service.KorailTalkApiService
 import org.sopt.korailtalk.data.util.safeApiCall
+import org.sopt.korailtalk.domain.model.DomainHomeBasicInfo
 import org.sopt.korailtalk.domain.model.DomainTrainInfo
 import org.sopt.korailtalk.domain.model.DomainTrainInfoRequest
 import org.sopt.korailtalk.domain.repository.KorailTalkRepository
@@ -12,7 +13,13 @@ import javax.inject.Inject
 class KorailTalkRepositoryImpl @Inject constructor(
     private val korailTalkService: KorailTalkApiService
 ) : KorailTalkRepository {
-    override suspend fun getTrainInfo(domainTrainInfoRequest: DomainTrainInfoRequest): Result<DomainTrainInfo> = safeApiCall {
-        return korailTalkService.getTrainInfo(domainTrainInfoRequest.toDto()).toModel()
+    override suspend fun getTrainInfo(domainTrainInfoRequest: DomainTrainInfoRequest): Result<DomainTrainInfo> =
+        safeApiCall {
+            return korailTalkService.getTrainInfo(domainTrainInfoRequest.toDto()).toModel()
+        }
+
+    override suspend fun getHomeBasicInfo(): Result<DomainHomeBasicInfo> = safeApiCall {
+        return korailTalkService.getHomeBasicInfo().toModel()
     }
+
 }
--- app/src/main/java/org/sopt/korailtalk/domain/repository/KorailTalkRepository.kt
+++ app/src/main/java/org/sopt/korailtalk/domain/repository/KorailTalkRepository.kt
@@ -1,8 +1,12 @@
 package org.sopt.korailtalk.domain.repository
 
+import org.sopt.korailtalk.domain.model.DomainHomeBasicInfo
 import org.sopt.korailtalk.domain.model.DomainTrainInfo
 import org.sopt.korailtalk.domain.model.DomainTrainInfoRequest
 
 interface KorailTalkRepository {
-    suspend fun getTrainInfo(domainTrainInfoRequest: DomainTrainInfoRequest) : Result<DomainTrainInfo>
+    suspend fun getTrainInfo(domainTrainInfoRequest: DomainTrainInfoRequest): Result<DomainTrainInfo>
+
+    suspend fun getHomeBasicInfo(): Result<DomainHomeBasicInfo>
+
 }
