0bb13a3fd81f7ad5e4b4ecc2c0ce7df8d0757d82
Kyu hyunSung <dmp100@naver.com>
2025-11-25 21:35:43 +0900

    fix/#36 : buildConfigField rollback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        //buildConfigField("String", "BASE_URL", properties["base.url"].toString())
-        buildConfigField("String", "BASE_URL", "\"${properties["base.url"]}\"")
+        buildConfigField("String", "BASE_URL", properties["base.url"].toString())
     }
 
     buildTypes {
