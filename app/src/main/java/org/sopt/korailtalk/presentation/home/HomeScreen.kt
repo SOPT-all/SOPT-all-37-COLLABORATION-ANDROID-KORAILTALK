2eee4cb0af6ae9545205057fd1d60275ba7e247a
oilbeaneda <20221444@sungshin.ac.kr>
2025-11-26 00:22:43 +0900

    feat/#47-홈 뷰모델/스크린 api 연동

--- app/src/main/java/org/sopt/korailtalk/presentation/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/korailtalk/presentation/home/HomeScreen.kt
@@ -1,28 +1,29 @@
 package org.sopt.korailtalk.presentation.home
 
+import android.widget.Toast
 import org.sopt.korailtalk.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.flow.collectLatest
 import org.sopt.korailtalk.core.common.util.preview.DefaultPreview
 import org.sopt.korailtalk.core.designsystem.component.topappbar.KorailTalkBasicTopAppBar
 import org.sopt.korailtalk.core.designsystem.theme.KorailTalkTheme
@@ -49,6 +50,20 @@ private fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     val uiState by viewModel.uiState.collectAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(viewModel.sideEffect) {
+        viewModel.sideEffect.collectLatest { effect ->
+            when (effect) {
+                is HomeSideEffect.ShowToast -> {
+                    Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    LaunchedEffect(Unit) {
+        viewModel.getHomeBasicInfo()
+    }
 
 
     Column(
@@ -108,7 +123,8 @@ private fun HomeScreen(
 
         )
         val items = items.toPersistentList()
-        EtcGridCards(items = items,
+        EtcGridCards(
+            items = items,
             Modifier.padding(horizontal = 16.dp)
         )
     }
--- app/src/main/java/org/sopt/korailtalk/presentation/home/HomeViewModel.kt
+++ app/src/main/java/org/sopt/korailtalk/presentation/home/HomeViewModel.kt
@@ -1,19 +1,56 @@
 package org.sopt.korailtalk.presentation.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.sopt.korailtalk.domain.repository.KorailTalkRepository
 import javax.inject.Inject
 
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
+    private val korailTalkRepository: KorailTalkRepository
 ) : ViewModel() {
+    //상태 관리
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+    private val _sideEffect = MutableSharedFlow<HomeSideEffect>()
+    val sideEffect: SharedFlow<HomeSideEffect> = _sideEffect.asSharedFlow()
+
+
+    //실제 서버 통신
+    fun getHomeBasicInfo() {
+        viewModelScope.launch {
+            korailTalkRepository.getHomeBasicInfo()
+                .onSuccess { response ->
+                    Log.d("HomeViewModel", "성공! 데이터: ${response}")
+                    //성공하면 받아온 데이터로 상태 교체
+                    _uiState.update { currentState ->
+                        currentState.copy(
+                            startStation = response.origin,
+                            endStation = response.destination
+                        )
+                    }
+                    _sideEffect.emit(HomeSideEffect.ShowToast("서버 통신 성공"))
+
+                }
+                .onFailure { error ->
+                    Log.e("HomeViewModel", "실패 : ${error.message}")
+                    //실패 시
+                    _sideEffect.emit(HomeSideEffect.ShowToast("에러 발생: ${error.message}"))
+                }
+
+        }
+    }
 
     // 역 위치를 서로 바꾸는 로직
     fun swapStations() {
@@ -25,5 +62,9 @@ class HomeViewModel @Inject constructor(
             )
         }
     }
+
 }
 
+sealed interface HomeSideEffect {
+    data class ShowToast(val message: String) : HomeSideEffect
+}
