3c127cbe09de523bb08a7633a375970ea8db10ab
oilbeaneda <20221444@sungshin.ac.kr>
2025-11-25 17:37:42 +0900

    feat/#47-mapper 작성
    dto ->model 연결

---
+++ app/src/main/java/org/sopt/korailtalk/data/mapper/HomeBasicInfoMapper.kt
@@ -0,0 +1,17 @@
+package org.sopt.korailtalk.data.mapper
+
+import org.sopt.korailtalk.data.dto.base.BaseResponse
+import org.sopt.korailtalk.data.dto.response.HomeBasicInfoResponseDto
+import org.sopt.korailtalk.domain.model.DomainHomeBasicInfo
+
+
+fun Result<BaseResponse<HomeBasicInfoResponseDto>>.toModel() : Result<DomainHomeBasicInfo> {
+    return this.mapCatching { baseResponse ->
+        val dto = baseResponse.data
+
+        DomainHomeBasicInfo(
+            origin = dto.origin,
+            destination = dto.destination
+        )
+    }
+}
